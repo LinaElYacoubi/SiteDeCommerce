10274e3741744fddf97f101923f6b645a788fcc0
LinaElYacoubi <linaelyacoubi60@gmail.com>
2025-07-01 19:01:30 -0400

    added more products

--- customvibes/src/data/product.js
+++ customvibes/src/data/product.js
@@ -1,25 +1,48 @@
-// data/products.js
 const productsData = [
   {
     id: 1,
     name: 'Custom Mug',
+    category: 'Mugs',
+    colors: ['White', 'Black', 'Pink'],
     descriptionLong: 'Personalize this classic mug with your own text or image. Dishwasher and microwave safe.',
     price: '13.99',
     image: require('../assets/mug.png')
   },
   {
     id: 2,
-    name: 'Personalized Tote',
+    name: 'Personalized Tote Bag',
+    category: 'Tote Bags',
+    colors: ['Beige', 'Pink', 'Black'],
     descriptionLong: 'Design your own eco-friendly tote bag, perfect for everyday use.',
     price: '17.99',
     image: require('../assets/tote.png')
   },
   {
     id: 3,
+    name: 'Custom Phone Case',
+    category: 'Phone Cases',
+    colors: ['White', 'Black', 'Blue'],
+    descriptionLong: 'Protect your phone in style! Add your name, photo, or favorite design.',
+    price: '19.99',
+    image: require('../assets/phonecase.png')
+  },
+  {
+    id: 4,
     name: 'Custom T-shirt',
+    category: 'T-shirts',
+    colors: ['White', 'Black', 'Beige'],
     descriptionLong: 'Create a T-shirt with your favourite phrase or photo, available in all sizes.',
     price: '22.99',
     image: require('../assets/tshirt.png')
+  },
+  {
+    id: 5,
+    name: 'Custom Hoodie',
+    category: 'Hoodies',
+    colors: ['White', 'Black', 'Grey'],
+    descriptionLong: 'Personalize a cozy hoodie for everyday comfort. Perfect for gifts or yourself!',
+    price: '34.99',
+    image: require('../assets/hoodie.png')
   }
 ];
 
